9c56f3fca5c2c9e35dc78c5b2f5c2d37891795d9
Wangzilin <wangzilinn@gmail.com>
2018-01-24 15:57:19 +0800

    已经完成链表二叉树，正在测试

--- 1/10.h
+++ 1/10.h
@@ -1,14 +1,14 @@
 #pragma once
 template <class K, class V>
-struct pair
+struct pair_rewrite
 {
     K key;
     V value;
-    pair(K theKey = 0, V theValue = 0) :key(theKey), value(theValue) {}
-    pair(const pair& thePair);
+    pair_rewrite(K theKey = 0, V theValue = 0) :key(theKey), value(theValue) {}
+    pair_rewrite(const pair_rewrite& thePair);
 };
 template <class K, class V>
-pair<K, V>::pair(const pair& thePair)
+pair_rewrite<K, V>::pair_rewrite(const pair_rewrite& thePair)
 {
     key = thePair.key;
     value = thePair.value;
@@ -17,20 +17,20 @@ template<class K, class V>
 class HashTable
 {
 private:
-    pair<K, V> ** table;
+    pair_rewrite<K, V> ** table;
     int divisor;
     int search(const K& theKey);//仅返回对应的下标
 public:
     HashTable(int theDivisor);//参数是除数
-    pair<K, V>* find(const K& theKey);
-    void insert(const pair<K, V>& thePair);
-    earse(const K& theKey);
+    pair_rewrite<K, V>* find(const K& theKey);
+    void insert(const pair_rewrite<K, V>& thePair);
+    void earse(const K& theKey);
 };
 template<class K, class V>
 HashTable<K, V>::HashTable(int theDivisor)
 {
     divisor = theDivisor;
-    table = new pair<K, V>[divisor];
+    table = new pair_rewrite<K, V>[divisor];
     fill(table, table + divisor, nullptr);
 }
 
@@ -61,7 +61,7 @@ int HashTable<K, V>::search(const K& theKey)//如果找到了，则返回找到的值得索引，
     //2.表满
 }
 template <class K, class V>
-pair<K, V>* HashTable<K, V>::find(const K& theKey)
+pair_rewrite<K, V>* HashTable<K, V>::find(const K& theKey)
 {
     int result = search(theKey);
     if (table[result]->key == theKey)
@@ -73,7 +73,7 @@ pair<K, V>* HashTable<K, V>::find(const K& theKey)
 }
 //插入规则：如果对应的Key值已经有数，那么替换为新的数，否则添加
 template <class K, class V>
-void HashTable<K, V>::insert(const pair<K, V>& thePair)
+void HashTable<K, V>::insert(const pair_rewrite<K, V>& thePair)
 {
     ////因为没有理解search函数的作用，所以这里又重写了一遍，以下代码很多都是和search重复的
     //int position = (int)(thePair.key) / divisor;
@@ -91,7 +91,7 @@ void HashTable<K, V>::insert(const pair<K, V>& thePair)
     //position = (position + 1) / divisor;//线性探查法解决冲突
     //while (table[position] != nullptr)
     //{
-    //    position = (position + 1) / divisor;
+    //    position = (position + 1) % divisor;
     //}
     //table[position]->key = thePair.key;
     //table[position]->value = thePair.value;
@@ -108,4 +108,30 @@ void HashTable<K, V>::insert(const pair<K, V>& thePair)
         return;
     }
     throw "哈希表已满";
+}
+template <class K, class V>
+void earse(const K& theKey)
+{
+    int position = search(theKey);
+    if (table[position] == nullptr)
+        return;
+    if (table[position]->key == theKey)//找到了对应位置
+    {
+        int origionalPosition = position;
+        int movePosition = position = (position + 1) % divisor;
+        while(position != origionalPosition)//防止循环超过一圈，position是当前位置，movePosition是下一个hash（key）和下标不匹配的位置
+        {
+            if (table[movePosition] == nullptr)
+                break;
+            while ((table[movePosition] != nullptr) && 
+                  ((int)hash(table[movePosition]->key) == movePosition))
+            {
+                movePosition = (movePosition + 1) % divisor;
+
+            }
+            table[position]->value = table[movePosition]->value;
+            position = movePosition;
+            movePosition = (movePosition + 1) % divisor;
+        }
+    }
 }
---
+++ 1/11.h
@@ -0,0 +1,140 @@
+#pragma once
+template<class T>
+struct binaryTreeNode
+{
+	T element;
+	binaryTreeNode* leftChild;
+	binaryTreeNode* rightChild;
+	binaryTreeNode(const T theElement) :element(theElement), leftChild(nullptr), rightChild(nullptr) {}
+	binaryTreeNode() :leftChild(nullptr), rightChild(nullptr) {}
+	binaryTreeNode(const T& theElement, binaryTreeNode* theLeftChild, binaryTreeNode* theRightChild)
+		:element(theElement), leftChild(theLeftChild), rightChild(theLeftChild) {}
+};
+//二叉树抽象类：
+template <class T>
+class binaryTree
+{
+public:
+	virtual ~binaryTree() {}
+	virtual bool empty() const = 0;
+	virtual int size() const = 0;
+	virtual void preOrder(void(*)(T*)) = 0;
+	virtual void inOrder(void(*)(T*)) = 0;
+	virtual void postOrder(void(*)(T*)) = 0;
+	virtual void levelOrder(void(*)(T*)) = 0;
+};
+//链表描述的二叉树：
+template<class T>
+class linkedBinaryTree :public binaryTree<binaryTreeNode<T>>
+{
+private:
+	binaryTreeNode<T> * root;
+	int treeSize;
+	static void (*visit)(binaryTreeNode<T>*);//静态类的static只需要再类内写，在外部不需要写
+	static void preOrder(binaryTreeNode<T>* t);
+	static void inOrder(binaryTreeNode<T>* t);
+	static void postOrder(binaryTreeNode<T> * t);
+	static void levelOrder(binaryTreeNode<T> *t);
+	static void dispose(binaryTreeNode<T> * t) { delete t };
+	static int height(binaryTreeNode<T> *t);
+public:
+	linkedBinaryTree() : root(nullptr), treeSize(0) {}
+	~linkedBinaryTree() { erase(); }
+	bool empty() const { return treeSize == 0; }
+	void preOrder(void(*theVisit)(binaryTreeNode<T>*))
+	{
+		visit = theVisit;
+		preOrder(root);
+	}
+	void inOrder(void(*theVisit)(binaryTreeNode<T>*))
+	{
+		visit = theVisit;
+		inOrder(root);
+	}
+	void postOrder(void(*theVisit)(binaryTreeNode<T>*))
+	{
+		visit = theVisit;
+		postOrder(root);
+	}
+	void levelOrder(void(*theVisit)(binaryTreeNode<T> *))
+	{
+		visit = theVisit;
+		levelOrder(root);
+	}
+	void earse()
+	{
+		postOrder(dispose);//使用后续遍历删除节点，不用保存节点信息
+		root = nullptr;
+		treeSize = 0;
+	}
+	int height() { return height(root); }
+};
+template<class T>
+void linkedBinaryTree<T>::preOrder(binaryTreeNode<T> *t)
+{
+	if (t != nullptr)
+	{
+		linkedBinaryTree<T>::visit(t);
+		preOrder(t->leftChild);
+		preOrder(t->rightChild);
+	}
+}
+template <class T>
+void linkedBinaryTree<T>::inOrder(binaryTreeNode<T> *t)
+{
+	if (t != nullptr)
+	{
+		inOrder(t->leftChild);
+		visit(t);
+		inOrder(t->rightChild);
+	}
+}
+template <class T>
+void linkedBinaryTree<T>::postOrder(binaryTreeNode<T> *t)
+{
+	if (t != nullptr)
+	{
+		postOrder(t->leftChild);
+		postOrder(t->rightChild);
+		visit(t);
+	}
+}
+template <class T>
+void linkedBinaryTree<T>::levelOrder(binaryTreeNode<T> *t)
+{
+	CircularArrayQuene<binaryTreeNode<T>*> quene;
+	while (t != nullptr)//当非空时才进入循环，只有这一个作用
+	{
+		if (!quene.empty())
+		{
+			t = quene.pop();
+		}
+		visit(t);
+		if (t->leftChild != nullptr)
+		{
+			quene.push(t->leftChild);
+		}
+		if (t->rightChild != nullptr)
+		{
+			quene.push(t->rightChild);
+		}
+		if (quene.empty())
+		{
+			return;
+		}
+	}
+}
+template <class T>
+int linkedBinaryTree<T>::height(binaryTreeNode<T>* t)
+{
+	if (t != nullptr)
+	{
+		int hl = height(t->leftChild);
+		int hr = height(t->rightChild);
+		return hl > hr ? ++hl : ++hr;
+	}
+	else
+	{
+		return 0;
+	}
+}
